8b1c7e793a9eafa4b01ab10d4ec03e1eb8cf12e4
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-07 19:01:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,12 +16,12 @@ def hopper
         }
      }
 
-
+  return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	programmer_hash[:grace_hopper][:known_for]
-  programmer_hash[:grace_hopper][:languages]
+	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
 	programmer_hash = 
  		{
         :grace_hopper => {
